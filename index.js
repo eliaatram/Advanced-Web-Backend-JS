ca2709cab2b5d2077aecf4e8d9507e9f187a1e44
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-08-03 22:51:05 +0200

    added template file structure & empty example to create branches later

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const express = require('express')
+const app = express();
+
+// SETUP MIDDLEWARE
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const cors = require('./utils/helpers/cors')
+
+// SETUP DOTENV
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors);
+app.use(helmet());
+app.enable('trust proxy');
+
+
+// ROUTES
+const userRoutes = require('./routes/user.routes');
+
+app.use('/users', userRoutes);
+
+app.listen(8000, function () {
+    console.log("Started application on port %d", 8000);
+});
---
+++ services/user.service.js
@@ -0,0 +1,10 @@
+const statusCodes = require('../utils/constants/statusCodes');
+const db = require('../database');
+
+const signup = async (req, res) => {
+
+}
+
+module.exports = {
+    signup
+}
