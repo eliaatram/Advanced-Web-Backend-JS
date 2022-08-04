36bb51b9be1f85e353f57b805595f604dc21d32f
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-08-04 23:12:25 +0200

    added movie post service

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const movies = require('../services/movies.service');
 
 router.get('/', movies.getMovies);
+router.post('/add/movie', movies.addMovie);
 
 module.exports = router
